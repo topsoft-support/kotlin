f5f1629b9ba43061097efd44a74c711edab30727
Roman Artemev <roman.artemev@jetbrains.com>
2020-01-27 18:41:07 +0300

    Fix native build

--- native/commonizer/src/org/jetbrains/kotlin/descriptors/commonizer/konan/NativeDistributionCommonizer.kt
+++ native/commonizer/src/org/jetbrains/kotlin/descriptors/commonizer/konan/NativeDistributionCommonizer.kt
@@ -214,7 +214,6 @@ class NativeDistributionCommonizer(
         val serializer = KlibMetadataMonolithicSerializer(
             languageVersionSettings = LanguageVersionSettingsImpl.DEFAULT,
             metadataVersion = KlibMetadataVersion.INSTANCE,
-            descriptorTable = EmptyDescriptorTable,
             skipExpects = false
         )
 
