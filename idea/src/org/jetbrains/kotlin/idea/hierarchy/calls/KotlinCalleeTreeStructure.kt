98606b2bea1b3baafbb102e6116065e637423909
Nikolay Krasko <goodwinnk@gmail.com>
2020-01-28 21:55:09 +0300

    201: Update hierarchy to new API (fix bugs introduced by the platform)
    193 plugins with old API are affected while being binary compatible.

--- idea/src/org/jetbrains/kotlin/idea/hierarchy/calls/KotlinCalleeTreeStructure.kt
+++ idea/src/org/jetbrains/kotlin/idea/hierarchy/calls/KotlinCalleeTreeStructure.kt
@@ -5,10 +5,7 @@
 
 package org.jetbrains.kotlin.idea.hierarchy.calls
 
-import com.intellij.ide.hierarchy.HierarchyNodeDescriptor
-import com.intellij.ide.hierarchy.HierarchyTreeStructure
 import com.intellij.ide.hierarchy.call.CallHierarchyNodeDescriptor
-import com.intellij.ide.hierarchy.call.CalleeMethodsTreeStructure
 import com.intellij.ide.util.treeView.NodeDescriptor
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
@@ -42,7 +39,7 @@ class KotlinCalleeTreeStructure(
     override fun buildChildren(nodeDescriptor: HierarchyNodeDescriptor): Array<Any> {
         if (nodeDescriptor is CallHierarchyNodeDescriptor) {
             val psiMethod = nodeDescriptor.enclosingElement as? PsiMethod ?: return ArrayUtil.EMPTY_OBJECT_ARRAY
-            return CalleeMethodsTreeStructure(myProject, psiMethod, scopeType).getChildElements(nodeDescriptor)
+            return createCalleeMethodsTreeStructure(myProject, psiMethod, scopeType).getChildElements(nodeDescriptor)
         }
 
         val element = nodeDescriptor.psiElement as? KtElement ?: return ArrayUtil.EMPTY_OBJECT_ARRAY
