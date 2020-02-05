f2369f00677a6d16226f3956233f138f0455c496
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2020-02-05 20:31:54 +0300

    Build: Make all dependencies on toolsJar compileOnly
    tools.jar differs between different versions of JDK reducing cache reuse
    so better to not leak it to other modules

--- compiler/cli/build.gradle.kts
+++ compiler/cli/build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
     compile(project(":kotlin-util-io"))
     compile(project(":compiler:ir.serialization.common"))
 
-    compile(toolsJar())
+    compileOnly(toolsJar())
     compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
     compileOnly(intellijDep()) { includeIntellijCoreJarDependencies(project) }
 
